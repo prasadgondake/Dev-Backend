664943b94eb223eda20837888f4e57e6cde30b82
Prasad Gondake <prasadgondake0910@gmail.com>
2023-08-22 23:07:49 +0530

    Updated commit

--- server.js
+++ server.js
@@ -37,19 +37,20 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
-const cors = require('cors'); // Import the cors package
+const cors = require('cors');
 
 const app = express();
 
-//Connect Database
+// Connect Database
 connectDB();
 
 app.use(express.json({ extended: false }));
 
-// Use cors middleware and specify your Netlify domain in the options
-app.use('*',
+// Use cors middleware and specify your domain in the options
+app.use(
+  '*',
   cors({
-    origin: 'https://main--inspiring-melomakarona-2f0a2a.netlify.app',
+    origin: 'https://your-domain.com', // Replace with your domain
   })
 );
 
@@ -60,7 +61,7 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  //Set static folder
+  // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
@@ -75,4 +76,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server started on port ${PORT}`)
 );
+
 
