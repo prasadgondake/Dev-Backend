ac4e04d2002eee1ef97ff5cd803b9d0a597d0655
Prasad Gondake <prasadgondake0910@gmail.com>
2023-08-22 22:32:42 +0530

    Updated commit

--- server.js
+++ server.js
@@ -1,36 +1,78 @@
-const { request } = require('express');
+// const { request } = require('express');
+// const express = require('express');
+// const connectDB = require('./config/db');
+// const path = require('path');
+
+// const app = express();
+
+// //Connect Database
+// connectDB();
+
+// app.use(express.json({ extended : false }));
+
+
+// app.use('/api/users' , require('./routes/api/users'));
+// app.use('/api/auth' , require('./routes/api/auth'));
+// app.use('/api/profile' , require('./routes/api/profile'));
+// app.use('/api/posts' , require('./routes/api/posts'));
+
+// // Serve static assets in production
+// if(process.env.NODE_ENV === 'production'){
+//     //Set static folder
+//     app.use(express.static('client/build'));
+
+//     app.get('*',(req, res)=> {
+//         res.sendFile(path.resolve(__dirname , 'client' , 'build' , 'index.html'));
+//         // res.send("Hello");
+//     });
+// }
+// // app.get('/',(req, res)=> {
+// //     res.send("Hello");
+// // });
+
+// const PORT=process.env.PORT || 5000;
+
+
+// app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
+const cors = require('cors'); // Import the cors package
 
 const app = express();
 
 //Connect Database
 connectDB();
 
-app.use(express.json({ extended : false }));
+app.use(express.json({ extended: false }));
 
+// Use cors middleware and specify your Netlify domain in the options
+app.use(
+  cors({
+    origin: 'https://main--inspiring-melomakarona-2f0a2a.netlify.app',
+  })
+);
 
-app.use('https://main--inspiring-melomakarona-2f0a2a.netlify.app/api/users' , require('./routes/api/users'));
-app.use('https://main--inspiring-melomakarona-2f0a2a.netlify.app/api/auth' , require('./routes/api/auth'));
-app.use('https://main--inspiring-melomakarona-2f0a2a.netlify.app/api/profile' , require('./routes/api/profile'));
-app.use('https://main--inspiring-melomakarona-2f0a2a.netlify.app/api/posts' , require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
-if(process.env.NODE_ENV === 'production'){
-    //Set static folder
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
 
-    app.get('*',(req, res)=> {
-        res.sendFile(path.resolve(__dirname , 'client' , 'build' , 'index.html'));
-        // res.send("Hello");
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, 'client', 'build', 'index.html')
+    );
+  });
 }
-// app.get('/',(req, res)=> {
-//     res.send("Hello");
-// });
 
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`)
+);
 
-app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
